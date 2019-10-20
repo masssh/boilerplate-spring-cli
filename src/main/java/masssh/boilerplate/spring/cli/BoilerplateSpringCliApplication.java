904d3002078a97da7c03d1d513a12b471702f31e
masssh <utm.ver.2.0@gmail.com>
2019-10-20 14:41:12 +0900

    Add redis

--- src/main/java/masssh/boilerplate/spring/cli/BoilerplateSpringCliApplication.java
+++ src/main/java/masssh/boilerplate/spring/cli/BoilerplateSpringCliApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoilerplateSpringCliApplication {
 
     public static void main(String[] args) {
-        System.out.println("hello!");
         SpringApplication.run(BoilerplateSpringCliApplication.class, args);
     }
 
